2d10b531e4735f31b104e881283e0ff08e628a93
Mafaruk <bagwannihal4@gmail.com>
2024-12-30 02:16:18 +0530

    Added compiler version in pom
    Signed-off-by: Mafaruk <bagwannihal4@gmail.com>

--- src/main/java/docker_java_simple_app/SimpleApp.java
+++ src/main/java/docker_java_simple_app/SimpleApp.java
@@ -2,7 +2,7 @@ package docker_java_simple_app;
 
 public class SimpleApp {
 	public static void main(String[] args) {
-		System.out.println("Hi, How are you");
+		System.out.println("Hi, How are you?");
 	}
 	
 
