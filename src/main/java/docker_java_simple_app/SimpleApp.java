60716196ae3d952a617cc72773a3b1219b34df10
Mafaruk4 <Mafaruk4@host.docker.internal>
2024-12-30 03:24:45 +0530

    made some changes to test docker env variables

--- src/main/java/docker_java_simple_app/SimpleApp.java
+++ src/main/java/docker_java_simple_app/SimpleApp.java
@@ -2,7 +2,8 @@ package docker_java_simple_app;
 
 public class SimpleApp {
 	public static void main(String[] args) {
-		System.out.println("Hi, How are you?");
+		String name = System.getenv("USER.NAME");
+		System.out.println("Hi "+name+", How are you?");
 	}
 	
 
